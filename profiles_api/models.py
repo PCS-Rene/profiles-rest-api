d96064595a2dc098f0e48a9bb50b01e8d76d435b
SF-Rene <rdesautels@successfinder.com>
2020-04-02 18:24:26 -0400

    Added custom user profile model, manager and migrations... Add some litle error

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -19,7 +19,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Create a new superuser with given details"""
         user = self.create_user(email, name, password)
         user.is_superuser = True  #is_superuser comes from PermissionMixin
@@ -38,7 +38,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
